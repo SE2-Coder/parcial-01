451353912c25470edccce53c288de5c73af901e2
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2022-07-27 22:57:50 -0500

    modified file

--- UserData.cpp
+++ UserData.cpp
@@ -8,11 +8,8 @@ struct Person{
         char nationality[15];
         char birthDay[9];
         int age = 0;
-        char profession[30];
-        char address[50];
-        char city[30];
-        
-    }person[5], *ptrPerson = person;
+                
+}person[5], *ptrPerson = person;
     
 void getData();
 
@@ -27,21 +24,17 @@ void getData()
 {
     for (int i = 0; i < 5; i++)
     {
+        //fflush:(stdin);
         cout << ">> Por favor digite su nombre: ";
         cin.getline((ptrPerson+i) -> name,30,'\n');
         cout << ">> Documento: ";
         cin.getline((ptrPerson+i) -> document,15,'\n');
         cout << ">> Nacionalidad: ";
         cin.getline((ptrPerson+i) -> nationality,15,'\n');
-        cout << ">> Fecha de nacimiento \nformato dd/mm/aa/: ";
+        cout << ">> Fecha de nacimiento formato dd/mm/aa/: ";
         cin.getline((ptrPerson+i) -> birthDay,15,'\n');
         cout << ">> Edad: ";
         cin >> (ptrPerson+i) -> age;
-        cout << "\n>> Profesion: ";
-        cin.getline((ptrPerson+i) -> profession,30,'\n');
-        cout << ">> Direccion: ";
-        cin.getline((ptrPerson+i) -> address,50,'\n');
-        cout << ">> Ciudad: ";
-        cin.getline((ptrPerson+i) -> city,30,'\n');
+        
     }
 }
