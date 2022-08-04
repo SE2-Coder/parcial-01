015b09e7f5fb4715cdeaa9fc9a34037e5a16216e
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2022-08-04 17:34:15 -0500

    transaction's header added

---
+++ Transactions.h
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+#include<ctime>
+using namespace std;
+
+namespace TrFuntions
+{
+    char** ptrTransactions; char**ptrTimeDate; int nRows, nCols = 50;
+
+    void getTimeNow ()
+    {
+        time_t timeNow = time(0);
+        char* dt = ctime(&timeNow);
+        //char* p = dt;
+        cout << dt;
+        
+        cout << "Numero de transacciones que quiere realizar: ";
+        cin >> nRows;
+        
+        ptrTransactions = new char*[nRows];
+        ptrTimeDate = new char*[nRows];
+        for (int i = 0; i < nRows; i++)
+        {
+            ptrTransactions[i] = new char[nCols];
+            ptrTimeDate[i] = new char[nCols];
+            
+        }   
+        
+        
+        for (int i = 0; i < nRows; i++)
+        {
+            cout << "\nIngrese el monto: $";
+            cin >> ptrTransactions[i];
+            ptrTimeDate[i] = dt;
+        }
+        cout << endl;
+    }
+
+    void showTransactions()
+    {
+        for (int i = 0; i < nRows; i++)
+        {
+            cout << "$" << ptrTransactions[i] << endl;
+            cout << ptrTimeDate[i] << endl;
+        }
+        
+    }
+}
