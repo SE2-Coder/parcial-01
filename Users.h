8c5cd48714060f1f9bced96fd671ee46a3f772d7
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2022-08-04 21:32:11 -0500

    vars file modified

--- Users.h
+++ Users.h
@@ -8,44 +8,44 @@ namespace systemUsers
 {
     char forms[][20] = {{"Nombre"},{"Documento"},{"Edad"},{"Profesion"},{"Nacionalidad"}};
     char **ptrNames, **ptrDocs, **ptrEdad, **ptrProfession, **ptrNationality;
-    int nCols = 30, nRows;
+    int uCols = 30, uRows;
 
     void getUserData()
     {
         cout << "Digite el numero de usuarios que desea registrar: ";
-        cin >> nRows;
+        cin >> uRows;
         
-        ptrNames = new char*[nRows];
-        ptrDocs = new char*[nRows];
-        ptrEdad = new char*[nRows];
-        ptrProfession = new char*[nRows];
-        ptrNationality = new char*[nRows];
+        ptrNames = new char*[uRows];
+        ptrDocs = new char*[uRows];
+        ptrEdad = new char*[uRows];
+        ptrProfession = new char*[uRows];
+        ptrNationality = new char*[uRows];
         
-        for (int i = 0; i < nRows; i++)
+        for (int i = 0; i < uRows; i++)
         {
-            ptrNames[i] = new char[nCols];
-            ptrDocs[i] = new char[nCols];
-            ptrEdad[i] = new char[nCols];
-            ptrProfession[i] = new char[nCols];
-            ptrNationality[i] = new char[nCols];    
+            ptrNames[i] = new char[uCols];
+            ptrDocs[i] = new char[uCols];
+            ptrEdad[i] = new char[uCols];
+            ptrProfession[i] = new char[uCols];
+            ptrNationality[i] = new char[uCols];    
         }
         
         cout << "\nDigite los datos del usuario o los usuarios que desea registrar.\n";
-        for (int i = 0; i < nRows; i++)
+        for (int i = 0; i < uRows; i++)
         {
             cin.ignore(100000,'\n');
             cout << "Nombre: "; 
-            cin.getline(ptrNames[i],nCols,'\n');
+            cin.getline(ptrNames[i],uCols,'\n');
             cout << "Documento: "; 
             cin >> ptrDocs[i];
             cout << "Edad: "; 
             cin >> ptrEdad[i];
             cin.ignore(100000,'\n');
             cout << "Profesion: "; 
-            cin.getline(ptrProfession[i],nCols,'\n');
+            cin.getline(ptrProfession[i],uCols,'\n');
             //cin.ignore(100000,'\n');
             cout << "Nationalidad: "; 
-            cin.getline(ptrNationality[i],nCols,'\n');
+            cin.getline(ptrNationality[i],uCols,'\n');
         }
         
     }
@@ -53,7 +53,7 @@ namespace systemUsers
     void showUserData(char**, int, int)
     {
         
-        for (int i = 0; i < nRows; i++)
+        for (int i = 0; i < uRows; i++)
         {
             cout << "Nombre: " << *(ptrNames+i) << endl;
             cout << "Documento: " << *(ptrDocs+i) << endl;
@@ -66,9 +66,9 @@ namespace systemUsers
     bool compareUserData(char**, int, int)
     {
         bool repeat = false;
-        for (int i = 0; i < nRows; i++)
+        for (int i = 0; i < uRows; i++)
         {
-            for (int j = 1; j < nRows; i++)
+            for (int j = 1; j < uRows; i++)
             {
                 if (*(ptrNames+i)==*(ptrNames+j) || *(ptrDocs+i)==*(ptrDocs+j))
                 {
