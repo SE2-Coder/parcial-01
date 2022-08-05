b986120e3817beee2775711cfe33ef5048592a03
SE2-Coder <criatiam.gutierrez@udea.edu.co>
2022-08-04 23:51:12 -0500

    Users.h file correct

--- Users.h
+++ Users.h
@@ -8,51 +8,93 @@ namespace systemUsers
 {
     char forms[][20] = {{"Nombre"},{"Documento"},{"Edad"},{"Profesion"},{"Nacionalidad"}};
     char **ptrNames, **ptrDocs, **ptrEdad, **ptrProfession, **ptrNationality;
+    char **ptrDbDocs;
     int uCols = 30, uRows;
 
+    bool compareUserData(char**, char**, int, int)
+    {
+        bool repeat = true;
+        int counter = 0;
+        for (int i = 0; i < uRows; i++)
+        {
+            for (int j = 0; j < uRows; j++)
+            {
+                if (ptrDocs[i] == ptrDbDocs[j])
+                {
+                    counter++;
+                }
+                
+            }
+        }
+        
+        if (counter == uRows)
+        {
+            repeat = false;
+        }
+        
+
+        return repeat;
+    }
+    
     void getUserData()
     {
-        cout << "Digite el numero de usuarios que desea registrar: ";
-        cin >> uRows;
-        
-        ptrNames = new char*[uRows];
-        ptrDocs = new char*[uRows];
-        ptrEdad = new char*[uRows];
-        ptrProfession = new char*[uRows];
-        ptrNationality = new char*[uRows];
-        
-        for (int i = 0; i < uRows; i++)
+        do
         {
-            ptrNames[i] = new char[uCols];
-            ptrDocs[i] = new char[uCols];
-            ptrEdad[i] = new char[uCols];
-            ptrProfession[i] = new char[uCols];
-            ptrNationality[i] = new char[uCols];    
-        }
+            cout << "\nDigite el numero de usuarios que desea registrar: ";
+            cin >> uRows;
+            
+            ptrNames = new char*[uRows];
+                
+            ptrDocs = new char*[uRows];
+            ptrDbDocs = new char*[uRows];
         
-        cout << "\nDigite los datos del usuario o los usuarios que desea registrar.\n";
-        for (int i = 0; i < uRows; i++)
-        {
-            cin.ignore(100000,'\n');
-            cout << "Nombre: "; 
-            cin.getline(ptrNames[i],uCols,'\n');
-            cout << "Documento: "; 
-            cin >> ptrDocs[i];
-            cout << "Edad: "; 
-            cin >> ptrEdad[i];
-            cin.ignore(100000,'\n');
-            cout << "Profesion: "; 
-            cin.getline(ptrProfession[i],uCols,'\n');
-            //cin.ignore(100000,'\n');
-            cout << "Nationalidad: "; 
-            cin.getline(ptrNationality[i],uCols,'\n');
-        }
+            ptrEdad = new char*[uRows];
+            ptrProfession = new char*[uRows];
+            ptrNationality = new char*[uRows];
+                
+            for (int i = 0; i < uRows; i++)
+            {
+                ptrNames[i] = new char[uCols];
+                
+                ptrDocs[i] = new char[uCols];
+                ptrDbDocs[i] = new char[uCols];
         
+                ptrEdad[i] = new char[uCols];
+                ptrProfession[i] = new char[uCols];
+                ptrNationality[i] = new char[uCols];    
+            }
+            
+            cout << "\nDigite los datos del usuario o los usuarios que desea registrar.\n";
+            for (int i = 0; i < uRows; i++)
+            {
+                cin.ignore(100000,'\n');
+                cout << "Nombre: "; 
+                cin.getline(ptrNames[i],uCols,'\n');
+                cout << "Documento: "; 
+                cin >> ptrDocs[i];
+        
+                ptrDbDocs[i] = ptrDocs[i]; 
+        
+                cout << "Edad: "; 
+                cin >> ptrEdad[i];
+                cin.ignore(100000,'\n');
+                cout << "Profesion: "; 
+                cin.getline(ptrProfession[i],uCols,'\n');
+                //cin.ignore(100000,'\n');
+                cout << "Nationalidad: "; 
+                cin.getline(ptrNationality[i],uCols,'\n');
+            } 
+            //compareUserData(ptrNames, uRows, uCols);
+            
+        }
+        while(compareUserData(ptrNames, ptrDbDocs, uRows, uCols) == true);
     }
 
     void showUserData(char**, int, int)
     {
         
+        cout << endl << "\t\t\tUSUARIOS ACTUALMENTE REGISTRADOS\n\n";
+
         for (int i = 0; i < uRows; i++)
         {
             cout << "Nombre: " << *(ptrNames+i) << endl;
@@ -60,25 +102,9 @@ namespace systemUsers
             cout << "Edad: " << *(ptrEdad+i) << endl;
             cout << "Profesion: " << *(ptrProfession+i) << endl;
             cout << "Nacionalidad: " << *(ptrNationality+i) << endl;
+            cout << endl;
         }
     }
 
-    bool compareUserData(char**, int, int)
-    {
-        bool repeat = false;
-        for (int i = 0; i < uRows; i++)
-        {
-            for (int j = 1; j < uRows; i++)
-            {
-                if (*(ptrNames+i)==*(ptrNames+j) || *(ptrDocs+i)==*(ptrDocs+j))
-                {
-                    repeat = true;
-                    break;
-                }
-                
-            }
-        }
-
-        return repeat;
-    }
+    
 }
